7cb90ce59b6a1e101ecb21f952964567cd3651a4
Kalasoftware <newsoftwarewala@gmail.com>
2025-03-15 17:48:03 +0530

    added unzip

--- railway-test/install-rclone.sh
+++ railway-test/install-rclone.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-echo "Installing rclone..."
+
+echo "Installing required dependencies..."
+apt-get update && apt-get install -y unzip
+
+echo "Downloading rclone..."
 curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+
+echo "Extracting rclone..."
 unzip rclone-current-linux-amd64.zip
 cd rclone-*-linux-amd64
+
+echo "Moving rclone to /usr/bin/..."
 cp rclone /usr/bin/
 chmod +x /usr/bin/rclone
+
 echo "Rclone Installed Successfully!"
